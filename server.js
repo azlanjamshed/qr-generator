1f116852e873ed249270e6ea330fde54087ee5c6
Azlan Jamshed <152954543+azlanjamshed@users.noreply.github.com>
2026-08-17 20:10:36 +0530

    Prepare QR generator for Render

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 require("dotenv").config();
 const path = require("path");
 
-
 const qrRoutes = require("./routes/qrRoutes");
 
 const app = express();
@@ -22,14 +21,16 @@ app.get("*name", (req, res) => {
   res.sendFile(path.join(publicDir, "index.html"));
 });
 const PORT = process.env.PORT || 8080;
-const MONGO_URI =
-  process.env.MONGO_URI 
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
   .connect(MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   })
   .catch((err) => {
     console.error("MongoDB connection error:", err.message);
